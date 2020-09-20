4000da348ce35779234f32cb28bfc17d872726cc
vikrantarora14 <vikrantarora14@gmail.com>
2020-09-20 15:39:41 +0100

    first checkin for the project

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+FROM nginx:latest
+COPY ./index.html /usr/share/nginx/html/index.html
+
+## Step 1:
+# Create a working directory
+#WORKDIR /app
+
+## Step 2:
+# Copy source code to working directory
+
+
+
+## Step 3:
+# Install packages from requirements.txt
+# hadolint ignore=DL3013
+#RUN pip install --upgrade pip &&\
+    #pip install --trusted-host pypi.python.org -r requirements.txt
+
+## Step 4:
+# Expose port 80
+#EXPOSE 80
+
+## Step 5:
+# Run app.py at container launch
+#CMD ["nginx", "helloworld.html"]
---
+++ run_kubernetes.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+# dockerpath=<>
+dockerpath="vikrantarora14/testcapstone"
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run capstoneindex\
+    --generator=run-pod/v1\
+    --image=$dockerpath
+    --port=80 --labels app=capstoneindex
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward capstoneindex 8080:80
